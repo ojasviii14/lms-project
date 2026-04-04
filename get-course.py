eff0d3dcc6073887f489a8eb8055121fcf1d4cdf
Alfin <alfinshaji198@gmail.com>
2026-04-04 13:33:15 +0530

    added cors headders and env varialble

--- get-course.py
+++ get-course.py
@@ -1,47 +1,37 @@
 import json
 import boto3
+import os
 from decimal import Decimal
-
-# Initialize DynamoDB
 dynamodb = boto3.resource('dynamodb')
-table = dynamodb.Table('courses')
-
-# Convert Decimal to int/float
+table = dynamodb.Table(os.environ['COURSES_TABLE'])
 def convert_decimal(obj):
     if isinstance(obj, list):
         return [convert_decimal(i) for i in obj]
     elif isinstance(obj, dict):
         return {k: convert_decimal(v) for k, v in obj.items()}
     elif isinstance(obj, Decimal):
-        return int(obj)  # convert Decimal to int
+        return int(obj)
     else:
         return obj
-
-
 def lambda_handler(event, context):
     try:
-        # Fetch data from DynamoDB
         response = table.scan()
         items = response.get('Items', [])
-
-        # Convert Decimal values
         items = convert_decimal(items)
-
-        # Debug print (optional)
         print("COURSES:", items)
-
         return {
             'statusCode': 200,
-            'body': json.dumps({
-                'courses': items
-            })
+            'headers': {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Headers': '*',
+                'Access-Control-Allow-Methods': '*'
+            },
+            'body': json.dumps({'courses': items})
         }
-
     except Exception as e:
         print("ERROR:", str(e))
         return {
             'statusCode': 500,
-            'body': json.dumps({
-                'error': str(e)
-            })
+            'headers': {'Access-Control-Allow-Origin': '*'},
+            'body': json.dumps({'error': str(e)})
         }
